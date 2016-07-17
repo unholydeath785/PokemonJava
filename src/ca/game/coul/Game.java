b0a77b3236167fd3b6fe500b9a9c40c08255403f
unholydeath785 <evan@coulson-orellana.com>
2016-07-16 17:30:17 -0700

    branch test

--- src/ca/game/coul/Game.java
+++ src/ca/game/coul/Game.java
@@ -9,16 +9,22 @@ import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
+import ca.game.coul.gfx.Spritesheet;
+
 public class Game extends Canvas implements Runnable {
 
 	private static final long serialVersionUID = 1L;
 	
-	public static final int WIDTH = 200;
-	public static final int HEIGHT = 150;
+	public static final int WIDTH = 340;
+	public static final int HEIGHT = 240;
 	public static final int SCALE = 3;
-	public static final String NAME = "Simon Says";
+	public static final String NAME = "Pokemon";
 	
 	private JFrame frame;
+	private Spritesheet tileSheet;
+	private Spritesheet npcSheet;
+	private Spritesheet characterSheet;
+	private Spritesheet pokemonSheet;
 	
 	public boolean running = false;
 	public int tickCount = 0;
@@ -52,7 +58,10 @@ public class Game extends Canvas implements Runnable {
 	}
 	
 	public void init() {
-		
+		this.characterSheet = new Spritesheet("/playersheet.jpg");
+		this.npcSheet = new Spritesheet("/npcsheet.png");
+		this.pokemonSheet = new Spritesheet("/pokemonsheet.png");
+		this.tileSheet = new Spritesheet("/tilesheet.png");
 	}
 	
 	public void run() {
@@ -115,7 +124,9 @@ public class Game extends Canvas implements Runnable {
 		Graphics g = bs.getDrawGraphics();
 		g.clearRect(0, 0, getWidth(), getHeight());
 		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, 600, 600);
+		g.fillRect(0, 0, 1020, 720);
+		
+		g.drawImage(this.tileSheet.getSprite(48, 0, 16, 16), 0, 0, null);
 		
 		bs.show();
 		g.dispose();
